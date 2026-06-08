4f4024496aa55833e1fc17990c6b32d1383a9134
UDay <uday.dev.bd@gmail.com>
2026-06-08 17:02:16 +0600

    fix: version update

--- packages/compose-react/src/services/auth.service.ts
+++ packages/compose-react/src/services/auth.service.ts
@@ -1,12 +1,12 @@
 import { HttpClient } from "@seliseblocks/blocks-kit-core/http";
 
-const http = new HttpClient({
-  baseURL: window?.process?.env?.BLOCKS_API_BASE_URL || "",
-  blocksKey: window?.process?.env?.BLOCKS_X_BLOCKS_KEY,
-});
-
 export class AuthService {
   logout() {
+    const http = new HttpClient({
+      baseURL: window?.process?.env?.BLOCKS_API_BASE_URL || "",
+      blocksKey: window?.process?.env?.BLOCKS_X_BLOCKS_KEY,
+    });
+
     const userBaseUrl = window.process?.env["userBaseUrl"] || "";
     return http.post(`${userBaseUrl}/api/auth/Logout`, {}, undefined, {
       absoluteUrl: true,
