bb7695d4aac392c2874a62afc37d5688ab48bc88
Md. Rifath Sahriar <Anubis1080p@gmail.com>
2026-06-08 20:22:55 +0600

    feat: add environment options and project detail component

--- packages/compose-react/src/lib/utils.ts
+++ packages/compose-react/src/lib/utils.ts
@@ -8,3 +8,15 @@ import { twMerge } from "tailwind-merge";
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+const pad = (num: number): string => num.toString().padStart(2, "0");
+export const formatFullDate = (date: Date, withoutTime?: boolean): string => {
+  const monthNames = [
+    "Jan", "Feb", "Mar", "Apr", "May", "Jun",
+    "Jul", "Aug", "Sep", "Oct", "Nov", "Dec",
+  ];
+  const dateStr = `${monthNames[date.getMonth()]} ${pad(date.getDate())}, ${date.getFullYear()}`;
+  const timeStr = `${pad(date.getHours())}:${pad(date.getMinutes())}`;
+  if (withoutTime) return dateStr;
+  return `${dateStr} at ${timeStr}`;
+};
