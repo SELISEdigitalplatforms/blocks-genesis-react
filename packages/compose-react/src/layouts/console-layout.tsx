b82f45ce386642ea689d4f8e74339be59bb15dd8
Sk. Zaber Ahmed <zaber.ahmed@selisegroup.com>
2026-06-09 19:43:08 +0600

    remove: unnecessary lines

--- packages/compose-react/src/layouts/console-layout.tsx
+++ packages/compose-react/src/layouts/console-layout.tsx
@@ -1,9 +1,5 @@
 import { ConsoleHeader } from "@/components/common/console-header/console-header";
 import type * as React from "react";
-// import {
-//   ImpersonationChecker,
-//   ImpersonationTerminator,
-// } from "../guards/protected-guard";
 
 export interface ConsoleLayoutProps {
   children: React.ReactNode;
@@ -11,13 +7,9 @@ export interface ConsoleLayoutProps {
 
 export function ConsoleLayout({ children }: ConsoleLayoutProps) {
   return (
-    // <ImpersonationChecker>
-    //   <ImpersonationTerminator>
     <div className="relative min-h-screen bg-[hsl(var(--surface-app))]">
       <ConsoleHeader />
       <main className={"pt-[59px]"}>{children}</main>
     </div>
-    //   </ImpersonationTerminator>
-    // </ImpersonationChecker>
   );
 }
