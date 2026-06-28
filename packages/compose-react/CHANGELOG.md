8e3a0d1cc09bdf12082fd5dda293d753df91bfe1
Sk. Zaber Ahmed <zaber.ahmed@selisegroup.com>
2026-06-28 15:39:32 +0600

    update: http client

--- packages/compose-react/CHANGELOG.md
+++ packages/compose-react/CHANGELOG.md
@@ -1,5 +1,11 @@
 # @seliseblocks/blocks-kit
 
+## 0.0.40
+
+### Patch Changes
+
+- updated http client
+
 ## 0.0.39
 
 ### Patch Changes
--- packages/compose-react/src/lib/http/types.ts
+++ packages/compose-react/src/lib/http/types.ts
@@ -14,12 +14,20 @@ export type RequestBody =
   | File
   | null
   | undefined;
+//TODO: Need to remove the commented-out code below after testing the new http client and found stable
+// export interface HttpClientOptions {
+//   skipBlocksKey?: boolean;
+//   withCredentials?: boolean;
+//   absoluteUrl?: boolean;
+//   skipTokenRotation?: boolean;
+// }
 
 export interface HttpClientOptions {
   skipBlocksKey?: boolean;
   withCredentials?: boolean;
   absoluteUrl?: boolean;
   skipTokenRotation?: boolean;
+  signal?: AbortSignal;
 }
 
 export interface RequestOptions extends HttpClientOptions {
@@ -38,9 +46,22 @@ export type RequestQueueItem<T> = BaseRequestQueueItem<T> & {
   requestOption: RequestOptions;
 };
 
+//TODO: Need to remove the commented-out code below after testing the new http client and found stable
+// export interface HttpClientConfig {
+//   baseURL: string | (() => string);
+//   blocksKey: string | (() => string);
+//   onTokenRefresh?: () => Promise<AuthTokenPair>;
+//   onUnauthorized?: (error: unknown) => void;
+// }
 export interface HttpClientConfig {
   baseURL: string | (() => string);
   blocksKey: string | (() => string);
   onTokenRefresh?: () => Promise<AuthTokenPair>;
   onUnauthorized?: (error: unknown) => void;
+  /** Paths that should never trigger an auth-failure redirect. Defaults to ["/login", "/signup"]. */
+  excludedPaths?: string[];
+  /** Where to send the user on an unrecoverable auth failure. Defaults to "/login". */
+  loginRedirectPath?: string;
+  /** Set false to fully delegate auth-failure handling to onUnauthorized instead of auto-redirecting. Defaults to true. */
+  autoRedirectOnAuthFailure?: boolean;
 }
