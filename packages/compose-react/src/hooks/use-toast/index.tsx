19ba3e01d9a7e864fc750bf77f2351b9c1f4bf22
Sk. Zaber Ahmed <zaber.ahmed@selisegroup.com>
2026-07-06 16:45:30 +0600

    update: toast removal time

--- packages/compose-react/src/hooks/use-toast/index.tsx
+++ packages/compose-react/src/hooks/use-toast/index.tsx
@@ -5,7 +5,7 @@ import type {
 import * as React from "react";
 
 const TOAST_LIMIT = 1;
-const TOAST_REMOVE_DELAY = 500;
+const TOAST_REMOVE_DELAY = 1000;
 type ToasterToast = ToastProps & {
   id: string;
   title?: React.ReactNode;
