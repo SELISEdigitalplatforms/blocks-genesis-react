24217ba82b537ab6550bec9fc4bd741a4c3521ba
Sk. Zaber Ahmed <zaber.ahmed@selisegroup.com>
2026-07-07 13:50:34 +0600

    chore: restructure folder

--- packages/compose-react/src/pages/dashboard/dashboard-overview-sections/index.ts
+++ packages/compose-react/src/pages/dashboard/dashboard-overview-sections/index.ts
@@ -2,3 +2,4 @@ export { CoreApiCard } from "./core-api";
 export { AppIntegrationCard } from "./api-integration-card";
 export { useSwaggerEndpoints } from "./use-swagger-endpoints";
 export { DomainsSection } from "./domain";
+export { GitCommandSnippet } from "./git-command-snippet";
