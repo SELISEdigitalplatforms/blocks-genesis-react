f11d02d2b357aa2710c63167e64771ebd25a8510
Sk. Zaber Ahmed <zaber.ahmed@selisegroup.com>
2026-07-14 18:01:59 +0600

    chore: export resolve env file

--- packages/compose-react/src/lib/index.ts
+++ packages/compose-react/src/lib/index.ts
@@ -1,5 +1,6 @@
 export * from "./http";
 export * from "./runtime-env";
+export * from "./resolve-env";
 export * from "./utils";
 export * from "./cookie-storage";
 export * from "./motion-presets";
