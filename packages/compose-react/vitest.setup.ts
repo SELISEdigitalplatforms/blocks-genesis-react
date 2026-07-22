79e59aaf50433c05d979bff5db80a305d9c4a087
rezwanx <rezwanxx@gmail.com>
2026-07-22 15:29:32 +0200

    test(kit): cover dialog, alert-dialog, sheet, select, dropdown-menu, command, timeline + pointer-capture polyfill

--- packages/compose-react/vitest.setup.ts
+++ packages/compose-react/vitest.setup.ts
@@ -48,6 +48,12 @@ if (!Element.prototype.scrollIntoView) {
   Element.prototype.scrollIntoView = () => {};
 }
 
+if (!Element.prototype.hasPointerCapture) {
+  Element.prototype.hasPointerCapture = () => false;
+  Element.prototype.setPointerCapture = () => {};
+  Element.prototype.releasePointerCapture = () => {};
+}
+
 afterEach(() => {
   cleanup();
 });
